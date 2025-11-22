adb38eadcc2b158683afef1f5b52c29a88295944
Raehan123 <nurrayhan62@gmail.com>
2025-11-23 01:45:49 +0700

    add jobsPage

---
+++ app/components/jobs/hero/page.tsx
@@ -0,0 +1,28 @@
+const Hero = () => {
+  return (
+    <section className="w-full h-[100vh] bg-linear-to-b from-[#63D9FA] to-[#F5F5F5] py-20 px-4">
+      <div className="max-w-4xl mx-auto text-center">
+        {/* Happy Users */}
+        <div className="flex items-center justify-center gap-2 mb-3">
+          <p className="text-white font-medium">5000+ Happy Users</p>
+        </div>
+
+        {/* Title */}
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white leading-tight">
+          Temukan Pekerjaan Yang Tepat,
+          <br />
+          Secara <span className="text-[#003C82]">Otomatis</span>
+        </h1>
+
+        {/* Subtitle */}
+        <p className="text-white max-w-2xl mx-auto mt-4">
+          Platform Kami Memanfaatkan AI Dan Analisis Data Untuk Menemukan
+          Peluang Kerja Terbaik Bagi Anda, Tanpa Proses Rumit, Agar Anda Bisa
+          Fokus Mengembangkan Diri Dan Karier.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
